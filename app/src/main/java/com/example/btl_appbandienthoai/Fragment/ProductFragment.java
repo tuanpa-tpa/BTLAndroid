22a509ed71c84a72b18dfb5d59069088d53bb20d
tuanpa.tpa <tuanpa.tpa@gmail.com>
2023-05-19 15:21:32 +0700

    search

--- app/src/main/java/com/example/btl_appbandienthoai/Fragment/ProductFragment.java
+++ app/src/main/java/com/example/btl_appbandienthoai/Fragment/ProductFragment.java
@@ -3,6 +3,8 @@ package com.example.btl_appbandienthoai.Fragment;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,6 +21,7 @@ import androidx.viewpager.widget.ViewPager;
 
 import com.example.btl_appbandienthoai.Adapter.ProductAdapter;
 import com.example.btl_appbandienthoai.Adapter.SlidePhotoAdapter;
+import com.example.btl_appbandienthoai.Class.Order;
 import com.example.btl_appbandienthoai.Class.Product;
 import com.example.btl_appbandienthoai.Class.SlidePhoto;
 import com.example.btl_appbandienthoai.Home;
@@ -39,7 +42,7 @@ import me.relex.circleindicator.CircleIndicator;
 public class ProductFragment extends Fragment {
 
     private Home home;
-//    để hẹn giờ cho slide chạy đúng
+    //    để hẹn giờ cho slide chạy đúng
     private Timer mTimer;
     private List<SlidePhoto> listSlidePhoto;
     private List<Product> listAllProduct;
@@ -59,7 +62,7 @@ public class ProductFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        mView =  inflater.inflate(R.layout.fragment_product,container,false);
+        mView = inflater.inflate(R.layout.fragment_product, container, false);
         // Khởi tạo các item
         initItem();
 
@@ -71,8 +74,9 @@ public class ProductFragment extends Fragment {
 
         return mView;
     }
+
     // Khởi tạo các item
-    private void initItem(){
+    private void initItem() {
         rcvProduct = mView.findViewById(R.id.rcv_product);
         viewPagerSlidePhoto = mView.findViewById(R.id.vp_slide_photo);
         circleIndicator = mView.findViewById(R.id.circle_indicator);
@@ -85,7 +89,7 @@ public class ProductFragment extends Fragment {
     }
 
     // Lấy Product để vào slide
-    private List<SlidePhoto> getListSlidePhoto(){
+    private List<SlidePhoto> getListSlidePhoto() {
         List<SlidePhoto> listSlidePhoto = new ArrayList<>();
         listSlidePhoto.add(new SlidePhoto(R.drawable.slide1));
         listSlidePhoto.add(new SlidePhoto(R.drawable.slide2));
@@ -94,8 +98,9 @@ public class ProductFragment extends Fragment {
         listSlidePhoto.add(new SlidePhoto(R.drawable.slide5));
         return listSlidePhoto;
     }
+
     // Set Adapter cho viewPagerSlidePhoto
-    private void setDataSlidePhotoAdapter(){
+    private void setDataSlidePhotoAdapter() {
         slidePhotoAdapter = new SlidePhotoAdapter(listSlidePhoto, this);
         viewPagerSlidePhoto.setAdapter(slidePhotoAdapter);
 //        set ảnh vào các chấm con của circle
@@ -108,11 +113,11 @@ public class ProductFragment extends Fragment {
     }
 
     // Auto chuyển các slide photo
-    private void autoSildeImage(){
-        if(listSlidePhoto == null || listSlidePhoto.isEmpty() || viewPagerSlidePhoto == null){
+    private void autoSildeImage() {
+        if (listSlidePhoto == null || listSlidePhoto.isEmpty() || viewPagerSlidePhoto == null) {
             return;
         }
-        if (mTimer == null){
+        if (mTimer == null) {
             mTimer = new Timer();
         }
         mTimer.schedule(new TimerTask() {
@@ -125,10 +130,10 @@ public class ProductFragment extends Fragment {
                         int totalItem = listSlidePhoto.size() - 1;
 
                         // Nếu item hiện tại chưa phải cuối cùng
-                        if(currentItem < totalItem){
+                        if (currentItem < totalItem) {
                             currentItem++;
                             viewPagerSlidePhoto.setCurrentItem(currentItem);
-                        }else {
+                        } else {
                             viewPagerSlidePhoto.setCurrentItem(0);
                         }
                     }
@@ -136,48 +141,96 @@ public class ProductFragment extends Fragment {
             }
 
             // xử lý thêm để set time
-        },500,3000 );
+        }, 500, 3000);
     }
 
     // Set Adapter cho rcvProduct
-    private void setDataProductAdapter(){
-
+    private void setDataProductAdapter() {
         GridLayoutManager gridLayoutManager = new GridLayoutManager(home, 2);
         rcvProduct.setLayoutManager(gridLayoutManager);
-
         productAdapter = new ProductAdapter();
-        productAdapter.setData(listAllProduct,home);
+        atcProductSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                Log.i("A", "change");
+            }
 
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.toString().isEmpty()) {
+                    getDataProduct();
+                } else {
+                    searchData(charSequence.toString());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                Log.i("C", "change");
+            }
+        });
         rcvProduct.setAdapter(productAdapter);
     }
+
     // Lấy dữ liệu Product từ FireBase
-    private List<Product> getDataProduct(){
+    private List<Product> getDataProduct() {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("DbProduct");
-
         List<Product> mListProduct = new ArrayList<>();
-
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                productAdapter.notifyDataSetChanged();
-                Log.i("Snapshot",snapshot.toString());
-                for (DataSnapshot data : snapshot.getChildren()){
+                mListProduct.clear();
+                Log.i("Snapshot", snapshot.toString());
+                for (DataSnapshot data : snapshot.getChildren()) {
                     Product product = data.getValue(Product.class);
                     product.setId(data.getKey());
                     mListProduct.add(product);
                 }
+                productAdapter.setData(mListProduct, home);
+                productAdapter.notifyDataSetChanged();
 //                setProductSearchAdapter(mListProduct);
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                Toast.makeText(getActivity(),"Không tải được dữ liệu từ firebase"
-                        +error.toString(),Toast.LENGTH_LONG).show();
-                Log.d("MYTAG","onCancelled"+ error.toString());
+                Toast.makeText(getActivity(), "Không tải được dữ liệu từ firebase"
+                        + error.toString(), Toast.LENGTH_LONG).show();
+                Log.d("MYTAG", "onCancelled" + error.toString());
             }
         });
         return mListProduct;
     }
 
+    private List<Product> searchData(String name) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("DbProduct");
+        List<Product> mListProduct = new ArrayList<>();
+        Log.i("AAAAA", "AAAA");
+        // Lấy thông tin order
+        myRef.orderByChild("productName").startAt(name)
+                .endAt(name+"\uf8ff")
+
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        Log.i("Snapshot", snapshot.toString());
+                        mListProduct.clear();
+                        for (DataSnapshot data : snapshot.getChildren()) {
+                            Log.i("data", data.getKey());
+                            Product product = data.getValue(Product.class);
+                            product.setId(data.getKey());
+                            mListProduct.add(product);
+                        }
+                        productAdapter.setData(mListProduct, home);
+                        productAdapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Toast.makeText(getContext(), "Không lấy được thông tin đơn hàng từ firebase", Toast.LENGTH_SHORT).show();
+                    }
+                });
+        return mListProduct;
+    }
 }
